b05e93e8cc46e2ec20dba4d5d1d61e83e3b249da
reina <bangtan1013jimin@gmail.com>
2026-02-25 11:14:11 +0800

    Fix React-Core non-modular header error in iOS Pods

--- plugins/withGrpcWorkaround.js
+++ plugins/withGrpcWorkaround.js
@@ -4,30 +4,30 @@ const withGrpcWorkaround = (config) => {
   return withPodfile(config, (config) => {
     let contents = config.modResults.contents;
 
-    // Add the workaround block inside the post_install hook
-    // If post_install already exists, Expo CLI usually adds it at the bottom.
-    // We can inject our hook.
     const grpcPatch = `
-  # GRPC Workaround for Firebase Static Frameworks linking issue
+  # Allow non-modular includes in all targets to fix RNFBApp / React-Core header issues
   installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
+    end
+    
+    # Specific workaround for Firebase Static Frameworks linking issue (gRPC)
     if target.name == 'gRPC-C++' or target.name == 'gRPC-Core'
       target.build_configurations.each do |config|
-        config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
         config.build_settings['DEFINES_MODULE'] = 'YES'
       end
     end
   end
 `;
 
-    // Try injecting before the end of the post_install block if it exists
     if (contents.includes("post_install do |installer|")) {
+      // Remove any previously injected gRPC workaround so we don't duplicate
+      contents = contents.replace(/# GRPC Workaround[\s\S]*?end\n  end/g, "");
+
       contents = contents.replace(
         /post_install do \|installer\|([\s\S]*?)end/g,
         (match, inner) => {
-          if (!inner.includes("gRPC-Core")) {
-            return `post_install do |installer|\n${inner}\n${grpcPatch}\nend`;
-          }
-          return match;
+          return `post_install do |installer|\n${inner}\n${grpcPatch}\nend`;
         }
       );
     } else {
