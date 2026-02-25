f9ef96437bd8538f54d97ac6f485b31be0f7144f
reina <bangtan1013jimin@gmail.com>
2026-02-25 14:35:10 +0800

    Added extra C99 flags

--- plugins/withGrpcWorkaround.js
+++ plugins/withGrpcWorkaround.js
@@ -6,9 +6,21 @@ const withGrpcWorkaround = (config) => {
 
     const grpcPatch = `
   # Allow non-modular includes in all targets to fix RNFBApp / React-Core header issues
+  # AND disable strict C99 implicit int errors enforced by newer Xcode versions
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
       config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
+      
+      # Suppress implicit int and warning-as-error safely
+      current_cflags = config.build_settings['OTHER_CFLAGS'] || '$(inherited)'
+      unless current_cflags.include?('-Wno-error=implicit-int')
+        extra_flags = ['-Wno-error=implicit-int', '-Wno-implicit-int', '-Wno-error=incompatible-pointer-types', '-Wno-error=int-conversion', '-Wno-error=deprecated-declarations']
+        if current_cflags.is_a?(Array)
+          config.build_settings['OTHER_CFLAGS'] = current_cflags + extra_flags
+        else
+          config.build_settings['OTHER_CFLAGS'] = current_cflags + ' ' + extra_flags.join(' ')
+        end
+      end
     end
     
     # Specific workaround for Firebase Static Frameworks linking issue (gRPC)
@@ -21,8 +33,7 @@ const withGrpcWorkaround = (config) => {
 `;
 
     if (contents.includes("post_install do |installer|")) {
-      // Remove any previously injected gRPC workaround so we don't duplicate
-      contents = contents.replace(/# GRPC Workaround[\s\S]*?end\n  end/g, "");
+      contents = contents.replace(/# Allow non-modular includes[\s\S]*?end\n  end/g, "");
 
       contents = contents.replace(
         /post_install do \|installer\|([\s\S]*?)end/g,
